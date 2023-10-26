5fe972774a4a933a8b5c31cb2146cbcdf3845280
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-26 15:30:08 -0300

    inicio

---
+++ 01-sintaxe-geral.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando PHP</title>
+
+</head>
+<body>
+    <h1>Expêriencia com PHP</h1>
+    <hr>
+
+    <?php
+    echo "<i>ola</i>";
+    echo 2023;
+    ?>
+
+</body>
+</html>
