81900d0de2d070609ca0c945af7e6a8093fe1020
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-26 17:04:59 -0300

    colocando o CSS no HTML pelo PHP

--- 02-variaveis-e-constantes.php
+++ 02-variaveis-e-constantes.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Variáveis e Constantes</title>
 
+    <style>
+        p { font-family: verdana; }
+
+        .destaque { 
+            background-color: yellow;
+            color: red;
+        }
+    </style>
+
 </head>
 <body>
     <h1>Variáveis e Constantes</h1>
@@ -22,10 +31,14 @@
 
     // Não dá certo, as variáveis são tratadas como texto
     echo '<p>Estamos no $curso em $ano</p>';
-    //---------------------------------------------------
-
+    //-------------------------------------------------------------------------------
+    //Usando CSS
+    echo "<p>Estamos no <span class=\"destaque\"> $curso</span> em $ano</p>";
+    
+    echo "<p>Estamos no <span class='destaque'> $curso</span> em $ano</p>";
+    
     // Sainda interpolada
-    echo "<p>Estamos no $curso em $ano</p>";
+    echo "<p>Estamos no $curso</span> em $ano</p>";
     
     // Saída com CONCATENAÇÃO
     echo '<p> estamos no '.$curso.' em '.$ano.'</p>';
@@ -33,9 +46,13 @@
     ?>
     <hr>
 
+    <!-- ---------------------------------------------------------------------------- -->
     <!-- Forma de Saída ABREVIADA/SIMPLIFICADA -->
-
     <p>Estamos no <?=$curso?> em <?=$ano?></p>
+    
+    <!-- usando o CSS NESSE TIPO -->
+    <p>Estamos no <span class="destaque"><?=$curso?></span> em <?=$ano?></p>
+    
     <hr>
     
     <?php
